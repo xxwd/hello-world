23a252bf8d98031b029d49e044eeb76fea82de38
xxwd <49076484+xxwd@users.noreply.github.com>
2020-07-30 22:36:17 -0400

    Create global.R

---
+++ global.R
@@ -0,0 +1,13 @@
+library(digest)
+library(DAL)
+library(shiny)
+library(sjmisc)
+library(DT)
+library(tidyverse) #set of package
+library(lubridate) #datetime
+library(magrittr) #use %>%
+
+library(rhandsontable) #datatable
+library(Log)
+library(shinyjs)
+library(highcharter) #charts
